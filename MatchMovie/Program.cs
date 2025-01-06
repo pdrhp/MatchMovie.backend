d2430215349df570d0c86f66a4c0e94cc422e5e4
pdrhp <pedrohsp2004@gmail.com>
2025-01-06 19:29:43 -0300

    feat: initial configuration of the project in program.cs
    - Adding SignalR
    - Adding Libs to manage Redis

--- MatchMovie/Program.cs
+++ MatchMovie/Program.cs
@@ -1,13 +1,24 @@
+using MatchMovie.Controllers;
+using MatchMovie.Hubs;
+using MatchMovie.Infrastructure.Persistence.Redis;
+using MatchMovie.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddRedisConfiguration(builder.Configuration);
+builder.Services.AddSignalRConfiguration();
+
+builder.Services.AddSingleton<IConnectionMapping, RedisConnectionMapping>();
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -15,30 +26,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthorization();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+app.MapControllers();
+app.MapHub<MatchHub>("/matchhub");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
