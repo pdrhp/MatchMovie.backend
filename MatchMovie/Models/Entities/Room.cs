7fa0f8f27f317a206ef1e73a9642778eb3317309
pdrhp <pedrohsp2004@gmail.com>
2025-01-06 19:30:03 -0300

    feat: creating room entity

---
+++ MatchMovie/Models/Entities/Room.cs
@@ -0,0 +1,17 @@
+﻿namespace MatchMovie.Models.Entities;
+
+public class Room
+{
+    public string Code { get; set; } = Guid.NewGuid().ToString("N")[..6].ToUpper();
+    public string HostConnectionId { get; set; }
+    public RoomSettings Settings { get; set; } = new();
+    public List<string> ParticipantsConnectionIds { get; set; } = new();
+    public RoomStatus Status { get; set; } = RoomStatus.WaitingToStart;
+}
+
+public enum RoomStatus
+{
+    WaitingToStart,
+    InProgress,
+    Finished
+}
---
+++ MatchMovie/Models/Entities/RoomSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace MatchMovie.Models.Entities;
+
+public class RoomSettings
+{
+    public List<string> Categories { get; set; } = new();
+    public int RoundDurationInMinutes { get; set; } = 3;
+    public int MaxParticipants { get; set; } = 10;
+}
